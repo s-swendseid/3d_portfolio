605a987534c6eb9591ebf1192affff258ec8f32b
s-swendseid <swendseidswag@gmail.com>
2023-12-07 10:32:23 -0700

    deleted unnessary files and removed space

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,9 +19,8 @@ const Hero = () => {
             Hi, I'm <span className="text-purple">Sarah!</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop frontend applications and user 
-            <br className="sm:block hidden" />
-            &nbsp; interfaces.
+            I develop frontend applications and user&nbsp;
+            <br className="sm:block hidden" />interfaces.
           </p>
         </div>
       </div>
--- src/components/index.js
+++ src/components/index.js
@@ -10,7 +10,6 @@ import About from "./About";
 import Tech from "./Tech";
 import Experience from "./Experience";
 import Works from "./Works";
-import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
 import CanvasLoader from "./Loader";
 
@@ -21,7 +20,6 @@ export {
   Tech,
   Experience,
   Works,
-  Feedbacks,
   Contact,
   CanvasLoader,
   EarthCanvas,
